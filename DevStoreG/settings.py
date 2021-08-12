67410143c0be712ed7129e698884fdb87314bc23
unknown <estuardoaf10@gmail.com>
2021-08-12 09:56:04 -0600

    Upgrade

--- DevStoreG/settings.py
+++ DevStoreG/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oihdtn4*8u!#s4zzmx^@a8xz**i)1*hegic2pfu60_wkwk2vp!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
