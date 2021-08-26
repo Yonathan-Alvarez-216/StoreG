61ca0a0d1b9cf2398b7002d40d7d90d8d87364df
unknown <estuardoaf10@gmail.com>
2021-08-25 19:19:37 -0600

    Update de Tablas de modelos

--- DevStoreG/settings.py
+++ DevStoreG/settings.py
@@ -74,22 +74,27 @@ WSGI_APPLICATION = 'DevStoreG.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
-import dj_database_url
-from decouple import config
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbstore',
+        'USER': 'devyona',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': 5432,
+   }
+}
+"""
+#import dj_database_url
+#from decouple import config
+
+#DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
+"""
 
 
 
--- apps/webstore/models.py
+++ apps/webstore/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class marca(models.Model):
+    pk_marca = models.AutoField(primary_key=True, null=False, blank=False)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    descripcion = models.TextField(null=False, blank=False)
+
+
+class categorias(models.Model):
+    pk_categorias = models.AutoField(primary_key=True, null=False, blank=False)
+    nombre = models.CharField(max_length=150, null=False, blank=False)
+    descripcion = models.TextField(null=False, blank=False)
+
+
+class foro(models.Model):
+    pk_foro = models.AutoField(primary_key=True, null=False, blank=False)
+    titulo = models.CharField(max_length=200, null=False, blank=False)
+    contenido = models.TextField(null=False, blank=False)
+    link = models.URLField(max_length=250)
+    fecha = models.DateField(auto_now=False, auto_now_add=True, null=False, blank=False)
+
+
+class productos(models.Model):
+    pk_productos = models.AutoField(primary_key=True, null=False, blank=False)
+    fk_categorias = models.ManyToManyField(categorias, blank=False)
+    fk_marca = models.ManyToManyField(marca, blank=False)
+    modelo = models.CharField(max_length=100, null=False, blank=False)
+    descripcion = models.TextField(null=False,blank=False)
+    fk_foro = models.ManyToManyField(foro, blank=False)
+
+
