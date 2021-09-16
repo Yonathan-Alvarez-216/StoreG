3fe26d151a1672318869e27f8186868990a7d015
unknown <estuardoaf10@gmail.com>
2021-09-16 10:03:41 -0600

    Deploy

--- DevStoreG/settings.py
+++ DevStoreG/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'DevStoreG.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,7 +95,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-"""
+
 
 
 
